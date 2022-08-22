4a1642501789788c778cb7b4153784da89f6aaca
Pavel Malinouski <chemodanius007@gmail.com>
2022-08-23 02:00:10 +0300

    updated as per reviewers' comments

--- WebDriverTask_Group_7/src/test/java/org/example/DeliverToTest.java
+++ WebDriverTask_Group_7/src/test/java/org/example/DeliverToTest.java
@@ -14,102 +14,86 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class DeliverToTest {
+    private WebDriver startingWebDriver() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://www.amazon.com/");
+        webDriver.manage().window().maximize();
+        return webDriver;
+    }
+
     @AfterTest
     public void shutDownWebDriver() {
         WebDriver webDriver = new ChromeDriver();
+        webDriver.close();
         webDriver.quit();
     }
 
     @Test
     public void verifyIfAmericanAddressIsPresent() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver webDriver = new ChromeDriver();
-
-        webDriver.get("https://www.amazon.com/");
-        webDriver.manage().window().maximize();
-        webDriver.navigate().refresh();
+        WebDriver webDriver = startingWebDriver();
 
         WebElement clickOnLogoDeliverTo = webDriver.findElement(By.id("nav-global-location-popover-link"));
         clickOnLogoDeliverTo.click();
 
-        Assert.assertTrue(clickOnLogoDeliverTo.isDisplayed());
-
-        WebElement enteringUsZipCode = webDriver.findElement(By.id("GLUXZipUpdateInput"));
+        WebElement enteringUsZipCode = new WebDriverWait(webDriver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("GLUXZipUpdateInput")));
         enteringUsZipCode.sendKeys("99801");
 
-        WebElement clickOnApplyButton = webDriver.findElement(By.id("GLUXZipUpdate"));
+        WebElement clickOnApplyButton = new WebDriverWait(webDriver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("GLUXZipUpdate")));
         clickOnApplyButton.click();
 
-        Assert.assertTrue(clickOnApplyButton.isDisplayed());
-
-        WebElement clickOnContinueButton = webDriver.findElement(By.xpath("//*[@id=\"a-popover-1\"]/div/div[2]/span/span"));
+        WebElement clickOnContinueButton = new WebDriverWait(webDriver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"a-popover-1\"]/div/div[2]/span/span")));
         clickOnContinueButton.click();
-
         webDriver.navigate().refresh();
 
         WebElement checkAddress = new WebDriverWait(webDriver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("glow-ingress-line2")));
-
         Assert.assertEquals(checkAddress.getText(), "Juneau 99801" + "\u200C");
     }
 
     @Test
-    public void verifyIfPolandIsPresent() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver webDriver = new ChromeDriver();
-
-        webDriver.get("https://www.amazon.com/");
-        webDriver.manage().window().maximize();
-        webDriver.navigate().refresh();
+    public void verifyIfPolandIsPresentInDropDown() {
+        WebDriver webDriver = startingWebDriver();
 
         WebElement clickOnLogoDeliverTo = webDriver.findElement(By.id("nav-global-location-popover-link"));
         clickOnLogoDeliverTo.click();
 
-        Assert.assertTrue(clickOnLogoDeliverTo.isDisplayed());
-
-        WebElement clickOnCountriesDropdown = new WebDriverWait(webDriver, Duration.ofSeconds(5))
+        WebElement clickOnCountriesDropdown = new WebDriverWait(webDriver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("GLUXCountryListDropdown")));
         clickOnCountriesDropdown.click();
 
         WebElement checkPoland = new WebDriverWait(webDriver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("GLUXCountryList_178")));
-
         Assert.assertEquals(checkPoland.getText(), "Poland");
     }
 
     @Test
     public void verifyShippingAddressToSelectedCountry() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver webDriver = new ChromeDriver();
-
-        webDriver.get("https://www.amazon.com/");
-        webDriver.manage().window().maximize();
+        WebDriver webDriver = startingWebDriver();
 
         WebElement clickOnLogoDeliverTo = webDriver.findElement(By.id("nav-global-location-popover-link"));
         clickOnLogoDeliverTo.click();
 
-        Assert.assertTrue(clickOnLogoDeliverTo.isDisplayed());
-
-        WebElement clickOnCountriesDropdown = new WebDriverWait(webDriver, Duration.ofSeconds(5))
+        WebElement clickOnCountriesDropdown = new WebDriverWait(webDriver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("GLUXCountryListDropdown")));
         clickOnCountriesDropdown.click();
 
-        WebElement clickOnSpain = new WebDriverWait(webDriver, Duration.ofSeconds(5))
+        WebElement clickOnSpain = new WebDriverWait(webDriver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("GLUXCountryList_209")));
-
         Assert.assertEquals(clickOnSpain.getText(), "Spain");
-
         clickOnSpain.click();
 
-        WebElement clickOnDoneButton = webDriver.findElement(By.xpath("//*[@id=\"a-popover-1\"]/div/div[2]/span/span/span/button"));
+        WebElement clickOnDoneButton = new WebDriverWait(webDriver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"a-popover-1\"]/div/div[2]/span/span/span/button")));
         clickOnDoneButton.click();
-
         webDriver.navigate().refresh();
 
-        WebElement clickOnAnyElement = webDriver.findElement(By.xpath("//img[contains(@alt,\"Keyboards\")]"));
+        WebElement clickOnAnyElement = new WebDriverWait(webDriver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[contains(@alt,\"Keyboards\")]")));
         clickOnAnyElement.click();
-
-        webDriver.navigate().refresh();
 
         WebElement clickOnFirstElementOnWebPage = new WebDriverWait(webDriver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By
@@ -118,7 +102,6 @@ public class DeliverToTest {
 
         WebElement verifyThatValueIsSpain = new WebDriverWait(webDriver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"amazonGlobal_feature_div\"]/span[1]")));
-
         Assert.assertEquals(verifyThatValueIsSpain.getText(), "No Import Fees Deposit & $13.74 Shipping to Spain");
     }
 }
