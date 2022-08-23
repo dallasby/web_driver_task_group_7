73a7ef11c92b59f23cf9a07e857db86a738c62fe
rupal <rupalik1986@gmail.com>
2022-08-23 16:44:07 +0200

    Updated chnages as per review comments

--- WebDriverTask_Group_7/src/test/java/org/example/SearchFunctionalityTest.java
+++ WebDriverTask_Group_7/src/test/java/org/example/SearchFunctionalityTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,20 +11,16 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class SearchFunctionalityTest {
     WebDriver driver;
     @BeforeClass
     public void init(){
-        System.setProperty("webdriver.chrome.driver",
-                "D:\\Rupali\\chromedriver.exe");
-
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.amazon.com/");
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    }
+        }
     @Test(priority = 1)
     public  void noProductTest(){
         WebElement searchBar = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
@@ -35,11 +32,10 @@ public class SearchFunctionalityTest {
         String actualTextStr = actualText.getText();
         String expectedText = "No results for";
         Assert.assertEquals(actualTextStr, expectedText);
-        driver.findElement(By.xpath("//a[@id='nav-logo-sprites']")).click();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    }
+            }
     @Test(priority = 2)
     public void laptopSearchTest() {
+        driver.findElement(By.xpath("//a[@id='nav-logo-sprites']")).click();
         WebElement searchBar = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
         searchBar.click();
         searchBar.sendKeys("Laptop");
@@ -50,7 +46,6 @@ public class SearchFunctionalityTest {
         String actualElementStrUpdated = actualElementStr.replaceAll("[^a-zA-Z]", "");
         String expectedElement = "Laptop";
         Assert.assertEquals(actualElementStrUpdated, expectedElement);
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
     }
     @Test(priority = 3)
@@ -59,16 +54,17 @@ public class SearchFunctionalityTest {
         // located element with contains()
         List<WebElement> WebElements = driver.findElements(By.xpath("//*[contains(text(),'Laptop')]"));
         int size = WebElements.size();
-        System.out.println(size);
-        for (int i = 1; i < size; i++) {
+         for (int i = 1; i < size; i++) {
             String s = WebElements.get(i).getText();
-            System.out.println("Text is " + s);
-        }
+                    }
 
         driver.findElement(By.xpath(
-                        "//*[@id=\'search\']/div[1]/div[1]/div/span[3]/div[2]/div[3]/div/div/div/div/div/div/div/div[2]/div/div/div[1]/h2/a/span"))
+                        "//span[normalize-space()='HP 15t-dy200 CTO 15.6\" FHD IPS Touchscreen Laptop, Intel i7-1165G7 (up to 4.7 GHz), 16GB (2 x 8GB) DDR4, 512GB SSD, Win 10, Natural Silver']"))
                 .click();
-        System.out.println(driver.getTitle());
+        String actualtitle = driver.getTitle();
+
+        String expectedtitle = "Amazon.com: HP 15t-dy200 CTO 15.6\" FHD IPS Touchscreen Laptop, Intel i7-1165G7 (up to 4.7 GHz), 16GB (2 x 8GB) DDR4, 512GB SSD, Win 10, Natural Silver : Electronics";
+        Assert.assertEquals(actualtitle, expectedtitle);
 
     }
     @AfterClass
